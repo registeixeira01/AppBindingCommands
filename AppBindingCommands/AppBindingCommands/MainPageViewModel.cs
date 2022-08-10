d79c9c6cd7d625bb8f209ab40cc1da72e73bc392
Régis Teixeira <regismarcondes2016@gmail.com>
2022-08-10 20:42:01 -0300

    Initial commit

---
+++ AppBindingCommands/AppBindingCommands/MainPageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppBindingCommands
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string name = string.Empty;// CTRL, R + E
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == null)
+                    return;
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+        public string DisplayName => $"Nome digitado: {Name}";
+
+
+
+        string displayMessage = string.Empty;
+        public string DisplayMessage
+        {
+            get => displayMessage;
+            set
+            {
+                if (displayMessage == null)
+                    return;
+
+                displayMessage = value;
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+        public ICommand ShowMessageCommand;
+
+        public void ShowMessage()
+        {
+            string dataProperty = Application.Current.Properties["dtAtual"].ToString();
+
+            DisplayMessage = $"Boa noite {Name}. Hoje é {dataProperty}.";
+        }
+
+        public MainPageViewModel()
+        {
+            ShowMessageCommand = new Command(ShowMessage);
+        }
+
+    }
+}
